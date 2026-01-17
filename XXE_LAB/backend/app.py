359ae9bff07316c601df8c17152a85d00996220a
Anandhu <connect.anandhukannan@gmail.com>
2026-01-17 17:12:19 +0530

    updated lab

--- XXE_LAB/backend/app.py
+++ XXE_LAB/backend/app.py
@@ -20,6 +20,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+@app.route("/labone")
+def labone():
+    return render_template("lab1.html")
+@app.route("/labtwo")
+def labtwo():
+    return render_template("lab2.html")
 
 @app.route("/xml", methods=["POST"])
 def parse_xml():
@@ -34,6 +40,39 @@ def parse_xml():
     root = etree.fromstring(xml_data, parser)
 
     return jsonify({"result": root.text})
+
+from lxml import etree
+from flask import request, jsonify
+
+class XXEResolver(etree.Resolver):
+    def resolve(self, url, pubid, context):
+        print("[XXE] Fetching:", url)
+        return self.resolve_filename(url, context)
+
+@app.route("/blind_xml", methods=["POST"])
+def parse_blind_xml():
+    xml_data = request.data
+
+    parser = etree.XMLParser(
+        load_dtd=True,
+        resolve_entities=True
+    )
+
+    parser.resolvers.add(XXEResolver())
+
+    root = etree.fromstring(xml_data, parser)
+
+    product_id = root.findtext("productId")
+
+    if not product_id or not product_id.isdigit():
+        return jsonify({"error": "Invalid product ID"}), 400
+
+    return jsonify({
+        "message": "Product processed successfully",
+        "productId": product_id
+    })
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
